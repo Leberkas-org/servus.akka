d76f492c31c439fde6888c135e8d8d65c2203f8d
st0o0 <64534642+st0o0@users.noreply.github.com>
2026-05-01 18:14:56 +0200

    Update xUnit V2 to V3 with mtp-v2

--- src/Servus.Akka.Tests/ActorRefInjectionTests.cs
+++ src/Servus.Akka.Tests/ActorRefInjectionTests.cs
@@ -1,7 +1,6 @@
 ﻿using Akka.Actor;
 using Akka.Hosting;
 using Akka.Hosting.TestKit;
-using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Servus.Akka.DependencyInjection;
@@ -10,7 +9,6 @@ namespace Servus.Akka.Tests;
 
 public class ActorRefInjectionTests : TestKit
 {
-    [UsedImplicitly]
     public class ResolvingTestActor : ReceiveActor
     {
         public ResolvingTestActor()
@@ -27,7 +25,7 @@ public class ActorRefInjectionTests : TestKit
     {
         var a = Host.Services.GetService<ActorRef<ResolvingTestActor>>();
         a.Tell("hello");
-        ExpectMsg("hello");
+        ExpectMsg("hello", cancellationToken: TestContext.Current.CancellationToken);
     }
 
     protected override void ConfigureHostBuilder(IHostBuilder builder)
