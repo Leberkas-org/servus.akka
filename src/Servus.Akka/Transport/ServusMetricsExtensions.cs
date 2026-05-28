8aa4a5f5a9b179b768173220be5c3d97d00530eb
Christian Dirnhofer <christian@dirnhofer.net>
2026-05-28 10:01:26 +0200

    chore: moved classes into own files

---
+++ src/Servus.Akka/Transport/ServusMetricsExtensions.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics.Metrics;
+using Servus.Core.Diagnostics;
+
+namespace Servus.Akka.Transport;
+
+internal static class ServusMetricsExtensions
+{
+    private static Histogram<double>? _dnsLookupDuration;
+    private static Histogram<double>? _socketConnectDuration;
+
+    public static Histogram<double> DnsLookupDuration(this ServusMetrics metrics)
+    {
+        return _dnsLookupDuration ??= metrics.Meter.CreateHistogram<double>(
+            "dns.lookup.duration",
+            unit: "s",
+            description: "Duration of DNS lookups in seconds");
+    }
+
+    public static Histogram<double> SocketConnectDuration(this ServusMetrics metrics)
+    {
+        return _socketConnectDuration ??= metrics.Meter.CreateHistogram<double>(
+            "network.socket.connect.duration",
+            unit: "s",
+            description: "Duration of socket connect operations in seconds");
+    }
+}
--- src/Servus.Akka/Transport/ServusTraceExtensions.cs
+++ src/Servus.Akka/Transport/ServusTraceExtensions.cs
@@ -1,30 +1,7 @@
 using System.Diagnostics;
-using System.Diagnostics.Metrics;
 using Servus.Core.Diagnostics;
 
 namespace Servus.Akka.Transport;
-
-internal static class ServusExtensions
-{
-    private static Histogram<double>? _dnsLookupDuration;
-    private static Histogram<double>? _socketConnectDuration;
-
-    public static Histogram<double> DnsLookupDuration(this ServusMetrics metrics)
-    {
-        return _dnsLookupDuration ??= metrics.Meter.CreateHistogram<double>(
-            "dns.lookup.duration",
-            unit: "s",
-            description: "Duration of DNS lookups in seconds");
-    }
-
-    public static Histogram<double> SocketConnectDuration(this ServusMetrics metrics)
-    {
-        return _socketConnectDuration ??= metrics.Meter.CreateHistogram<double>(
-            "network.socket.connect.duration",
-            unit: "s",
-            description: "Duration of socket connect operations in seconds");
-    }
-}
 
 internal static class ServusTraceExtensions
 {
